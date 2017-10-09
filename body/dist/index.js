b7a8ca091b6363c44546e300026606736f751368
安迪帆 <0@00010101.com>
2017-10-09 13:37:31 +0800

    Update index.js
    test

--- body/dist/index.js
+++ body/dist/index.js
@@ -1,145 +1,53 @@
-;(function(main){
-	window["MBody"] = main;
-})((function(){
-	var mods = {};
-	function require(id){
-		return mods[id];
-	}
-	function define(id, factory){
-		var module = {exports: {}};
-		factory(require, module.exports, module);
-		mods[id] = module.exports;
-	}
 
-	function $all_in_one_css_injector(content){
-		var styleNode = document.createElement("div");
-		styleNode.innerHTML = '<br /><style type="text/css">' + content + '</style>';
-		return styleNode = document.getElementsByTagName("head")[0].appendChild(styleNode.lastChild);
-	}
-	
-	
-	// main.styl
-	define("1", function(require, exports, module){
-		module.exports = $all_in_one_css_injector('.body,\n.body .pre,\n.body .current,\n.body .next {\n  position: absolute;\n  left: 0;\n  right: 0;\n  top: 0;\n  bottom: 0;\n  background-color: #fff;\n}\n.body {\n  overflow: hidden;\n}\n.body .current {\n  z-index: 1;\n}\n.body-scroll .pre {\n  transform: translateX(-33%);\n  box-shadow: 0 0 0 rgba(0,0,0,0);\n}\n.body-scroll .current {\n  transform: translateX(0);\n  box-shadow: 0 0 30px rgba(0,0,0,0.3);\n}\n.body-scroll .next {\n  transform: translateX(100%);\n  box-shadow: 0 0 0 rgba(0,0,0,0);\n}\n.body-scroll.scroll-next .animate {\n  transition: transform 0.5s cubic-bezier(0, 0, 0, 1), box-shadow 0.5s cubic-bezier(0, 1, 0, 1);\n}\n.body-scroll.scroll-pre .animate {\n  transition: transform 0.5s cubic-bezier(0, 0, 0, 1), box-shadow 0.5s cubic-bezier(1, 0, 1, 0);\n}\n.body-scroll.scroll-pre .next {\n  z-index: 2;\n}\n.body-opacity .animate {\n  transition: opacity 0.5s linear;\n}\n.body-opacity .pre,\n.body-opacity .next {\n  opacity: 0;\n}\n.body-opacity .current {\n  opacity: 1;\n}\n');
-	});
+import * as babel from "babel-core";
 
-	// main.tpl
-	define("2", function(require, exports, module){
-		module.exports = '<div class="body">\r\n\t<div class="pre"></div>\r\n\t<div class="current"></div>\r\n\t<div class="next"></div>\r\n</div>';
-	});
+// require(string)
+function isRequire(path){
+    const node = path.node;
 
-	// index.js
-	define("0", function(require, exports, module){
-		require("1");
-		var template = require("2");
-		
-		function replaceClass(classList, c1, c2){
-			c1.split(" ").forEach(function(className){
-				classList.remove(className);
-			});
-			c2.split(" ").forEach(function(className){
-				classList.add(className);
-			});
-		}
-		
-		function Body(config){
-			this.config = config || {};
-			this.config.animate = this.config.animate === false ? false : (this.config.animate || "scroll");
-		
-			var docFragment = document.createElement("div");
-			docFragment.innerHTML = template;
-		
-			this.container = docFragment.children[0];
-			this.panels = this.container.children;
-		
-			document.body.appendChild(this.container);
-			docFragment = null;
-		}
-		
-		Body.prototype = {
-			pre: function(animate){
-				if(this.animateHandler){
-					clearTimeout(this.animateHandler);
-					this.animateHandler = null;
-				}
-		
-				this.config.animate && this.container.classList.remove("body-" + this.config.animate);
-				animate = animate === false ? false : (animate || this.config.animate);
-				animate && this.container.classList.add("body-" + animate);
-		
-				setTimeout(function(){
-					var animateClass = "";
-		
-					if(animate !== false){
-						replaceClass(this.container.classList, animate + "-next", animate + "-pre");
-						animateClass = " animate";
-					}
-					
-					for(var i = 0, l = this.panels.length, classList; i < l; i ++){
-						classList = this.panels[i].classList;
-						if(classList.contains("pre")){
-							replaceClass(classList, "pre", "current" + animateClass);
-						}else if(classList.contains("current")){
-							replaceClass(classList, "current", "next" + animateClass);
-						}else if(classList.contains("next")){
-							replaceClass(classList, "next animate", "pre");
-						}
-					}
-		
-					this.animateHandler = setTimeout(function(){
-						this.container.classList.remove(animate + "-pre");
-						this.animateHandler = null;
-					}.bind(this), 5000);
-				}.bind(this), 1);
-			},
-			next: function(animate){
-				if(this.animateHandler){
-					clearTimeout(this.animateHandler);
-					this.animateHandler = null;
-				}
-		
-				this.config.animate && this.container.classList.remove("body-" + this.config.animate);
-				animate = animate === false ? false : (animate || this.config.animate);
-				animate && this.container.classList.add("body-" + animate);
-		
-				setTimeout(function(){
-					var animateClass = "";
-		
-					if(animate !== false){
-						replaceClass(this.container.classList, animate + "-pre", animate + "-next");
-						animateClass = " animate";
-					}
-		
-					for(var i = 0, l = this.panels.length, classList; i < l; i ++){
-						classList = this.panels[i].classList;
-						if(classList.contains("pre")){
-							replaceClass(classList, "pre animate", "next");
-						}else if(classList.contains("current")){
-							replaceClass(classList, "current", "pre" + animateClass);
-						}else if(classList.contains("next")){
-							replaceClass(classList, "next", "current" + animateClass);
-						}
-					}
-		
-					this.animateHandler = setTimeout(function(){
-						this.container.classList.remove(animate + "-next");
-						this.animateHandler = null;
-					}.bind(this), 5000);
-				}.bind(this), 1);
-			},
-			get: function(name){
-				name = name || "next";
-		
-				for(var i = 0, l = this.panels.length; i < l; i ++){
-					if(this.panels[i].classList.contains(name)){
-						return this.panels[i];
-					}
-				}
+    return node.callee.type === "Identifier"
+            && node.callee.name === "require"
+            && node.arguments[0]
+            && node.arguments[0].type === "StringLiteral"
+            && !path.scope.hasBinding("require");
+}
+
+export default parseDeps(code){
+    var deps = [];
+    // 提取依赖
+    babel.transform(code, {
+        plugins: [
+            () => {
+                visitor: {
+                    CallExpression(path){
+                        if(isRequire(path)){
+		            let mod = path.node.arguments[0].value;
+		            if(!deps.contains(mod)){
+				deps.push(mod);
+			    }
 			}
-		};
-		
-		module.exports = Body;
-	});
+		    }
+		}
+	    }
+	]
+    });
 
-	return require("0");
-})());
+    return deps;
+}
+
+const code = `
+    const obj = {
+        a: 'test a '
+    }
+    const a = require('./a');
+    const b = require('./b');
+
+    const c = 'this is a test item';
+
+    const d = require('../d');
+
+    console.log(a, b, c, d);
+`;
+
+var deps = parseDeps(code);
+console.log(deps);
